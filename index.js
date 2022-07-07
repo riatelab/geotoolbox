69dc024881219fdc7832992794fe253b806908bb
neocarto <nicolas.lambert@cnrs.fr>
2022-07-07 12:08:13 +0200

    fix: module config

--- index.js
+++
@@ -1,8 +0,0 @@
-export { add } from "./src/add.js";
-export { filter } from "./src/filter.js";
-export { keep } from "./src/keep.js";
-export { remove } from "./src/remove.js";
-export { table } from "./src/table.js";
-export { subset } from "./src/subset.js";
-export { head } from "./src/head.js";
-export { tail } from "./src/tail.js";
---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { add } from "./add.js";
+export { filter } from "./filter.js";
+export { keep } from "./keep.js";
+export { remove } from "./remove.js";
+export { table } from "./table.js";
+export { subset } from "./subset.js";
+export { head } from "./head.js";
+export { tail } from "./tail.js";
