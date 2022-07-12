0153737eb25e1db4a48774566808720fe98489e2
neocarto <nicolas.lambert@cnrs.fr>
2022-07-12 11:39:23 +0200

    feat: union by id

--- src/gis/union.js
+++ src/gis/union.js
@@ -2,7 +2,32 @@ import { topology } from "topojson-server";
 import { merge } from "topojson-client";
 const topojson = Object.assign({}, { topology, merge });
 
-export function union(x) {
+export function union(x, options = {}) {
+  if (options.id != null && options.id != undefined) {
+    let id = options.id;
+    let arr = Array.from(new Set(world.features.map((d) => d.properties[id])));
+    let features = [];
+    arr.forEach((myid) => {
+      let geo = {
+        type: "FeatureCollection",
+        features: world.features.filter((d) => d.properties[id] == myid),
+      };
+      //return geo;
+      let topo = topojson.topology({ foo: geo });
+      features.push({
+        type: "Feature",
+        properties: { id: myid },
+        geometry: topojson.merge(topo, topo.objects.foo.geometries),
+      });
+    });
+
+    return {
+      type: "FeatureCollection",
+      features: features,
+    };
+  } else {
+  }
+
   let topo = topojson.topology({ foo: x });
   return {
     type: "FeatureCollection",
