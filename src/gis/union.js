fdb99f35bc1777ae2ce788cda5512e91d78aee91
neocarto <nicolas.lambert@cnrs.fr>
2022-10-06 14:55:51 +0200

    feat : union by id

--- src/gis/union.js
+++ src/gis/union.js
@@ -2,22 +2,47 @@ import * as jsts from "jsts/dist/jsts";
 
 // Ajouter id comme option comme dans aggregate
 
-export function union(x) {
+export function union(x, options = {}) {
   let reader = new jsts.io.GeoJSONReader();
   let data = reader.read(x);
 
-  let geom = data.features[0].geometry;
+  if (options.id != null && options.id != undefined) {
+    // Union by id
+    let ids = Array.from(
+      new Set(data.features.map((d) => d.properties[options.id]))
+    );
 
-  for (let i = 1; i < data.features.length; i++) {
-    geom = geom.union(data.features[i].geometry);
+    let result = [];
+    ids.forEach((d) => {
+      let features = data.features.filter((e) => e.properties[options.id] == d);
+      let geom = features[0].geometry;
+      for (let i = 1; i < features.length; i++) {
+        geom = geom.union(features[i].geometry);
+      }
+
+      result.push({
+        type: "Feature",
+        properties: { id: d },
+        geometry: new jsts.io.GeoJSONWriter().write(geom),
+      });
+    });
+
+    return {
+      type: "FeatureCollection",
+      features: result,
+    };
+  } else {
+    // Union all
+    let geom = data.features[0].geometry;
+    for (let i = 1; i < data.features.length; i++) {
+      geom = geom.union(data.features[i].geometry);
+    }
+
+    const result = new jsts.io.GeoJSONWriter().write(geom);
+
+    return {
+      type: "FeatureCollection",
+      features: [{ type: "Feature", properties: {}, geometry: result }],
+    };
   }
-
-  data.features.map((d) => d.geometry);
-
-  const result = new jsts.io.GeoJSONWriter().write(geom);
-
-  return {
-    type: "FeatureCollection",
-    features: [{ type: "Feature", properties: {}, geometry: result }],
-  };
 }
