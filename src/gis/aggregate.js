6d5867a55359a9c496da107820d9d503021204ec
neocarto <nicolas.lambert@cnrs.fr>
2022-12-08 16:25:47 +0100

    feat: aggregate works on dots and lines

--- src/gis/aggregate.js
+++ src/gis/aggregate.js
@@ -1,11 +1,12 @@
+import { type } from "../utils/type.js";
+import { dissolve } from "./dissolve.js";
 import { topology } from "topojson-server";
 import { merge } from "topojson-client";
 const topojson = Object.assign({}, { topology, merge });
-import { featurecollection } from "../helpers/featurecollection.js";
+import { featurecollection } from "../utils/featurecollection.js";
 
 export function aggregate(x, options = {}) {
   x = featurecollection(x);
-
   if (options.id != null && options.id != undefined) {
     let id = options.id;
     let arr = Array.from(new Set(x.features.map((d) => d.properties[id])));
@@ -16,11 +17,35 @@ export function aggregate(x, options = {}) {
         features: x.features.filter((d) => d.properties[id] == myid),
       };
       //return geo;
-      let topo = topojson.topology({ foo: geo });
+
+      let geom;
+      if (type(x) == "poly") {
+        let topo = topojson.topology({ foo: geo });
+        geom = topojson.merge(topo, topo.objects.foo.geometries);
+      }
+
+      if (type(x) == "line") {
+        geom = {
+          type: "MultiLineString",
+          coordinates: dissolve(geo).features.map(
+            (d) => d.geometry.coordinates
+          ),
+        };
+      }
+
+      if (type(x) == "point") {
+        geom = {
+          type: "MultiPoint",
+          coordinates: dissolve(geo).features.map(
+            (d) => d.geometry.coordinates
+          ),
+        };
+      }
+
       features.push({
         type: "Feature",
         properties: { id: myid },
-        geometry: topojson.merge(topo, topo.objects.foo.geometries),
+        geometry: geom,
       });
     });
 
@@ -29,17 +54,35 @@ export function aggregate(x, options = {}) {
       features: features,
     };
   } else {
-  }
+    let geom;
+    if (type(x) == "poly") {
+      let topo = topojson.topology({ foo: x });
+      geom = topojson.merge(topo, topo.objects.foo.geometries);
+    }
 
-  let topo = topojson.topology({ foo: x });
-  return {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        properties: {},
-        geometry: topojson.merge(topo, topo.objects.foo.geometries),
-      },
-    ],
-  };
+    if (type(x) == "line") {
+      geom = {
+        type: "MultiLineString",
+        coordinates: dissolve(x).features.map((d) => d.geometry.coordinates),
+      };
+    }
+
+    if (type(x) == "point") {
+      geom = {
+        type: "MultiPoint",
+        coordinates: dissolve(x).features.map((d) => d.geometry.coordinates),
+      };
+    }
+
+    return {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {},
+          geometry: geom,
+        },
+      ],
+    };
+  }
 }
