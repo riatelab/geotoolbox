0413d915f7a8c47e2c658d73bd71f5eec35c6e83
neocarto <nicolas.lambert@cnrs.fr>
2022-12-08 16:27:23 +0100

    code organization

--- src/helpers/km2deg.js
+++
@@ -1,6 +0,0 @@
-export function km2deg(distance) {
-  const earthRadius = 6371.0088;
-  const radians = distance / earthRadius;
-  const degrees = radians % (2 * Math.PI);
-  return (degrees * 180) / Math.PI;
-}
